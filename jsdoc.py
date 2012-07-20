d208e5822d849dd2767f21e8486bce0160a76a82
tschaub <tim.schaub@gmail.com>
2012-07-20 15:12:20 -0600

    Update to account for new structure.

--- jsdoc.py
+++ jsdoc.py
@@ -2,7 +2,7 @@ import sys
 from jstools import jst
 
 len(sys.argv) > 3 or sys.exit("Usage: doc.py <repo> <template> <output>")
-print sys.argv[1]
+
 parser = jst.DocParser({
   "root": sys.argv[1], "template": sys.argv[2], "output": sys.argv[3]
 })
